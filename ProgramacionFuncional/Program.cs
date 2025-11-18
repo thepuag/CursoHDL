b63a76bc96958b877959df4c8553d03e6edd2011
Noe <noe.m.garc@gmail.com>
2025-11-18 16:30:37 +0100

    Fun orden superior

--- ProgramacionFuncional/Program.cs
+++ ProgramacionFuncional/Program.cs
@@ -1,8 +1,10 @@
-﻿DateTime datito = new DateTime(2025,11,12);
+﻿using System.Threading.Channels;
+
+DateTime datito = new DateTime(2025,11,12);
 
 Console.WriteLine(CreadoMasUno(datito));
 Console.WriteLine(ActualMasUno());
-await Task.Delay(5000);
+await Task.Delay(1000);
 Console.WriteLine(ActualMasUno());
 DateTime CreadoMasUno(DateTime time)
 {
@@ -16,20 +18,71 @@ Cositas heineken = new Cositas{
 
 Cositas mercedes = new Cositas("Mercedes", 2);
 
-ModificarNombreMayusculas(heineken);
-Console.WriteLine($"El nombre de heineken despues de usar ModificarNombreMayusculas > {heineken.Nombre}");
-ModificarNombreMAy2(mercedes);
-Console.WriteLine($"El nombre de heineken despues de usar ModificarNombreMAy2 > {mercedes.Nombre}");
-void ModificarNombreMayusculas(Cositas objeto) 
+ModificarObjetoOriginal(heineken);
+//Console.WriteLine($"El nombre de heineken despues de usar ModificarNombreMayusculas > {heineken.Nombre}");
+
+//Console.WriteLine($"El nombre de mercedes antes de usar metodo > {mercedes.Nombre}");
+//Console.WriteLine($"Le paso el objeto mercedes y me devuelve una copia con el nombre en mayusculas > {NoModificaObjeto(mercedes).Nombre}");
+//Console.WriteLine($"El nombre de mercedes despues de usar metodo > {mercedes.Nombre}");
+
+var t = NoModificaObjeto;
+
+Action<Cositas> d = ModificarObjetoOriginal;
+
+Console.WriteLine("Aqui debajo esta el ejemplo de Action");
+
+d(mercedes);
+
+Action<string> muestra = Console.WriteLine;
+muestra("Hola");
+//Console.WriteLine(t(mercedes).Nombre);
+Action<String> muestraNombre = nombre => muestra($"Hola {nombre}");
+muestraNombre("Paquito");
+
+Func<int, int, int> mult = (a, b) => a*b;
+muestra(mult(4, 2).ToString());
+
+Func<int, int, string> multString = (a, b) =>
+{
+    var resultado = (a*b).ToString();
+    return resultado;
+};
+
+muestra(multString(10, 50));
+
+List<int> listaNumeros = new List<int>() { 2,7,12,15,20,21,22};
+
+var numbers2 = Filtrar(listaNumeros, number => number % 2 == 0);
+
+foreach (var item in numbers2)
+{
+    Console.WriteLine(item);
+}
+
+List<int> Filtrar(List<int> listaEnteros, Func<int,bool> condicion) 
+{
+    var listaResultante = new List<int>();
+    foreach (var elemento in listaEnteros)
+    {
+        if(condicion(elemento))
+        {
+            listaResultante.Add(elemento);
+        }
+    }
+    return listaResultante;
+}
+
+void ModificarObjetoOriginal(Cositas objeto) 
 {
     objeto.Nombre = objeto.Nombre.ToUpper();
     Console.WriteLine(objeto.Nombre);
 }
-void ModificarNombreMAy2(Cositas objeto)
+Cositas NoModificaObjeto(Cositas objeto)
 {
-    var newObjeto = objeto.Nombre.ToUpper(); 
+    Cositas newObjeto = new Cositas(); 
+    newObjeto.Nombre = objeto.Nombre.ToUpper(); 
     
-    Console.WriteLine(newObjeto);
+    return newObjeto;
 }
 
 class Cositas
